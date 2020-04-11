050d13f4cd11aa1d2e15c9fe0e5a57ba55899b83
Gonçalo Marques <goncalo.m.marques@tecnico.ulisboa.pt>
2020-04-11 18:26:05 +0100

    Adding Listing and Terminating EC2 Instances

--- scripts/module_02/manage-ec2-instance.js
+++ scripts/module_02/manage-ec2-instance.js
@@ -1,20 +1,40 @@
 // Imports
-// TODO: Import the aws-sdk
+const AWS = require('aws-sdk')
 
-// TODO: Configure region
+AWS.config.update({ region: 'us-east-1'})
 
 // Declare local variables
-// TODO: Create an ec2 object
+const ec2 = new AWS.EC2()
 
 function listInstances () {
-  // TODO: List instances using ec2.describeInstances()
+    return new Promise((resolve, reject) => {
+        ec2.describeInstances({}, (err, data) => {
+            if (err) reject(err)
+            else {
+                resolve(data.Reservations.reduce((i, r) => {
+                    return i.concat(r.Instances)
+                }, []))
+            }
+        })
+    })
 }
 
 function terminateInstance (instanceId) {
-  // TODO: Terminate an instance with a given instanceId
+    const params = {
+        InstanceIds: [
+            instanceId
+        ]
+    }
+
+    return new Promise((resolve, reject) => {
+        ec2.terminateInstances(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
 }
 
-listInstances()
-.then(data => console.log(data))
-// terminateInstance()
-// .then(data => console.log(data))
+//listInstances()
+//.then(data => console.log(data))
+ terminateInstance('i-08021ae2f430c40f7')
+ .then(data => console.log(data))
