f723ff8f2a81cfb2559ecc8c8cda55ed5c7f0326
Gonçalo Marques <goncalo.m.marques@tecnico.ulisboa.pt>
2020-04-11 18:48:16 +0100

    Adding Creating AMI

--- scripts/module_02/create-ami.js
+++ scripts/module_02/create-ami.js
@@ -1,14 +1,24 @@
 // Imports
-// TODO: Import the aws-sdk
+const AWS = require('aws-sdk')
 
-// TODO: Configure region
+AWS.config.update({ region: 'us-east-1'})
 
 // Declare local variables
-// TODO: Create an ec2 object
+const ec2 = new AWS.EC2()
 
-createImage('<instanceid>', 'hamsterImage')
+createImage('i-053e64710e1251759', 'hamsterImage')
 .then(() => console.log('Complete'))
 
 function createImage (seedInstanceId, imageName) {
-  // TODO: Implement AMI creation
+    const params = {
+        InstanceId: seedInstanceId,
+        Name: imageName,
+    }
+
+    return new Promise((resolve, reject) => {
+        ec2.createImage(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
 }
