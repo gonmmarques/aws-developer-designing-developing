d278d98cf840ac85f05f58ef0e07ce8ba2e91bff
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-11 18:14:52 +0100

    Creating EC2 Instance

--- scripts/module_02/create-ec2-instance.js
+++ scripts/module_02/create-ec2-instance.js
@@ -30,7 +30,7 @@ createSecurityGroup(sgName)
 function createSecurityGroup (sgName) {
   const params = {
     Description: sgName,
-    Group: sgName
+    GroupName: sgName
   }
 
   return new Promise((resolve, reject) => {
@@ -84,5 +84,22 @@ function createKeyPair (keyName) {
 }
 
 function createInstance (sgName, keyName) {
-  // TODO: create ec2 instance
+  const params = {
+    ImageId: 'ami-0323c3dd2da7fb37d',
+    InstanceType: 't2.micro',
+    KeyName: keyName,
+    MaxCount: 1,
+    MinCount: 1,
+    SecurityGroups: [
+        sgName
+    ],
+    UserData: 'IyEvYmluL2Jhc2gNCmN1cmwgLS1zaWxlbnQgLS1sb2NhdGlvbiBodHRwczovL3JwbS5ub2Rlc291cmNlLmNvbS9zZXR1cF8xMi54IHwgc3VkbyBiYXNoIC0NCnN1ZG8geXVtIGluc3RhbGwgLXkgbm9kZWpzDQpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQNCmNkIGhiZmwNCm5wbSBpDQpucG0gcnVuIHN0YXJ0'
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.runInstances(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
