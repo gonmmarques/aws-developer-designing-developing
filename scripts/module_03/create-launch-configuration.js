8113c8c5680c4662e463d277c23f53fe6e91255a
Gonçalo Marques <goncalo.m.marques@tecnico.ulisboa.pt>
2020-04-12 17:06:41 +0100

    Adding Creating Launch Configuration

--- scripts/module_03/create-launch-configuration.js
+++ scripts/module_03/create-launch-configuration.js
@@ -1,10 +1,10 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: add your region */' })
+AWS.config.update({ region: 'us-east-1'})
 
 // Declare local variables
-// TODO: Create an autoscaling object
+const autoScaling = new AWS.AutoScaling()
 
 const lcName = 'hamsterLC'
 const roleName = 'hamsterLCRole'
@@ -16,5 +16,22 @@ helpers.createIamRole(roleName)
 .then(data => console.log(data))
 
 function createLaunchConfiguration (lcName, profileArn) {
-  // TODO: Create a launch configuration
+    const params = {
+        IamInstanceProfile: profileArn,
+        ImageId: 'ami-014dea3e162978eb4',
+        InstanceType: 't2.micro',
+        LaunchConfigurationName: lcName,
+        KeyName: keyName,
+        SecurityGroups: [
+            sgName
+        ],
+        UserData: 'IyEvYmluL2Jhc2gNCmN1cmwgLS1zaWxlbnQgLS1sb2NhdGlvbiBodHRwczovL3JwbS5ub2Rlc291cmNlLmNvbS9zZXR1cF8xMi54IHwgc3VkbyBiYXNoIC0NCnN1ZG8geXVtIGluc3RhbGwgLXkgbm9kZWpzDQpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQNCmNkIGhiZmwNCm5wbSBpDQpucG0gcnVuIHN0YXJ0'
+    }
+
+    return new Promise((resolve, reject) => {
+        autoScaling.createLaunchConfiguration(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
 }
