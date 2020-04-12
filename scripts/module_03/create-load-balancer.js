7828ef6a3426005da2c46e31359ceb8fdc0bd9ea
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-12 17:29:44 +0100

    Adding Creating ELB

--- scripts/module_03/create-load-balancer.js
+++ scripts/module_03/create-load-balancer.js
@@ -2,16 +2,17 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1'})
 
 // Declare local variables
-// TODO: Create a new ELBv2 object
+const elbv2 = new AWS.ELBv2()
 const sgName = 'hamsterELBSG'
 const tgName = 'hamsterTG'
 const elbName = 'hamsterELB'
-const vpcId = '/* TODO: Add your VPC Id */'
+const vpcId = 'vpc-d10e60ab'
 const subnets = [
-  /* TODO: Add two subnets */
+  'subnet-44d88718',
+  'subnet-985b09ff'
 ]
 
 helpers.createSecurityGroup(sgName, 80)
@@ -30,7 +31,20 @@ helpers.createSecurityGroup(sgName, 80)
 .then((data) => console.log(data))
 
 function createLoadBalancer (lbName, sgId) {
-  // TODO: Create a load balancer
+  const params = {
+    Name: lbName,
+    Subnets: subnets,
+    SecurityGroups: [
+        sgId
+    ]
+  }
+
+  return new Promise((resolve, reject) => {
+    elbv2.createLoadBalancer(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
 
 function createTargetGroup (tgName) {
