75d4b640735a621aaace619b3516ca45537da4fb
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-12 17:34:46 +0100

    Adding Creating Auto Scaling Group

--- scripts/module_03/create-auto-scaling.js
+++ scripts/module_03/create-auto-scaling.js
@@ -1,21 +1,40 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: add your region */' })
+AWS.config.update({ region: 'us-east-1'})
 
 // Declare local variables
 const autoScaling = new AWS.AutoScaling()
 const asgName = 'hamsterASG'
 const lcName = 'hamsterLC'
 const policyName = 'hamsterPolicy'
-const tgArn = '/* TODO: get target group ARN */'
+const tgArn = 'arn:aws:elasticloadbalancing:us-east-1:773771193701:targetgroup/hamsterTG/48aa0215a22f08d9'
 
 createAutoScalingGroup(asgName, lcName)
 .then(() => createASGPolicy(asgName, policyName))
 .then((data) => console.log(data))
 
 function createAutoScalingGroup (asgName, lcName) {
-  // TODO: Create an auto scaling group
+    const params = {
+        AutoScalingGroupName: asgName,
+        AvailabilityZones: [
+            'us-east-1a',
+            'us-east-1b'
+        ],
+        TargetGroupARNs: [
+            tgArn
+        ],
+        LaunchConfigurationName: lcName,
+        MaxSize: 2,
+        MinSize: 1
+    }
+
+    return new Promise((resolve, reject) => {
+        autoScaling.createAutoScalingGroup(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
 }
 
 function createASGPolicy (asgName, policyName) {
