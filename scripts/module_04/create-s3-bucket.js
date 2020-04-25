8739de4dcb777f5861f436cb74d4dba4d2e9821a
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-25 16:12:00 +0100

    Adding Creating an S3 Bucket

--- scripts/module_04/create-s3-bucket.js
+++ scripts/module_04/create-s3-bucket.js
@@ -1,18 +1,24 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
-// TODO: Create new s3 object
+const s3 = new AWS.S3()
 
-createBucket('hamster-bucket/* TODO: Add a unique identifier */')
+createBucket('hamster-bucket-gm')
 .then((data) => console.log(data))
 
 function createBucket (bucketName) {
-  // TODO: Define params object
+  const params = {
+    Bucket:  bucketName,
+    ACL: 'public-read'
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Create s3 bucket
+    s3.createBucket(params, (err, data) => {
+      if (err) reject(err)
+      else resolve (data)
+    })
   })
 }
