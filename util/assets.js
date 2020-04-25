f9a7d9e25c90a82aa9f3c2109e08f4fbe12b7852
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-25 16:46:05 +0100

    Adding Uploading objects to S3

--- util/assets.js
+++ util/assets.js
@@ -1,4 +1,4 @@
-const base = '/public'
+const base = 'https://hamster-bucket-gm.s3.amazonaws.com'
 module.exports = {
   hamster1: base + '/images/hamster-1-final.png',
   hamster2: base + '/images/hamster-2-final.png',
