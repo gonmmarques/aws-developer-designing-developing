e777a72e21c4e7fde745cb7ab03cf601315d4b34
Gonçalo Marques <goncalo.m.marques@tecnico.ulisboa.pt>
2020-04-25 17:52:17 +0100

    Adding Creating an ElastiCache Cluster

--- scripts/module_05/create-redis-cluster.js
+++ scripts/module_05/create-redis-cluster.js
@@ -4,16 +4,27 @@ const helpers = require('./helpers')
 
 AWS.config.update({ region: 'us-east-1' })
 
-// TODO: Create an elasticache object
+const ec = new AWS.ElastiCache()
 
 helpers.createSecurityGroup('hamster_redis_sg', 6379)
 .then(sgId => createRedisCluster('hamster', sgId))
 .then(data => console.log(data))
 
 function createRedisCluster (clusterName, sgId) {
-  // TODO: Create params object
+  const params = {
+    CacheClusterId: clusterName,
+    CacheNodeType: 'cache.t2.micro',
+    Engine: 'redis',
+    NumCacheNodes: 1,
+    SecurityGroupIds: [
+      sgId
+    ]
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Create cache cluster
+    ec.createCacheCluster(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
