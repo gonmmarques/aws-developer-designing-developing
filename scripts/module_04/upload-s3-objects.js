09519c49e256eb92fbc40eb4ba8427465a02fe5f
Gonçalo Marques <goncalo.m.marques@tecnico.ulisboa.pt>
2020-04-25 16:46:05 +0100

    Adding Uploading objects to S3

--- scripts/module_04/upload-s3-objects.js
+++ scripts/module_04/upload-s3-objects.js
@@ -2,26 +2,34 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 const s3 = new AWS.S3()
-const bucketName = '/* TODO: Make this bucket name match what you created */'
+const bucketName = 'hamster-bucket-gm'
 
 helpers.getPublicFiles()
 .then(files => uploadS3Objects(bucketName, files))
 .then(data => console.log(data))
 
 function uploadS3Objects (bucketName, files) {
-  // TODO: Define putObject params object
+  const params = {
+    Bucket: bucketName,
+    ACL: 'public-read'
+  }
 
   const filePromises = files.map((file) => {
     const newParams = Object.assign({}, params, {
-      // TODO: Add individual file params
+      Body: file.contents,
+      Key: file.name,
+      ContentType: helpers.getContentType(file.name)
     })
 
     return new Promise((resolve, reject) => {
-      // TODO: Put objects in S3
+      s3.putObject(newParams, (err, data) => {
+        if (err) reject(err)
+        else resolve(data)
+      })
     })
   })
 
