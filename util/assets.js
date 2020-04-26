aa78f051b2efed0993e16d94f725be5d5a9737da
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-26 18:15:51 +0100

    Adding Creating a CloudFront Distribution

--- util/assets.js
+++ util/assets.js
@@ -1,4 +1,4 @@
-const base = 'https://s3.amazonaws.com/hamster-bucket-gm'
+const base = 'https://d2mpyctltpymmp.cloudfront.net'
 module.exports = {
   hamster1: base + '/images/hamster-1-final.png',
   hamster2: base + '/images/hamster-2-final.png',
