90bce26d9dab52f08694ef7d5888949d1f3f531b
Gonçalo Marques <goncalo.m.marques@tecnico.ulisboa.pt>
2020-04-26 18:40:17 +0100

    Configuring S3 as Static Website

--- scripts/module_07/configure-s3-website.js
+++ scripts/module_07/configure-s3-website.js
@@ -1,18 +1,28 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 const s3 = new AWS.S3()
 
-configureS3Site('/* TODO: Add your S3 bucket name */')
+configureS3Site('hamster-bucket-gm')
 .then(data => console.log(data))
 
 function configureS3Site (bucketName) {
-  // TODO: Create params const object
+  const params = {
+    Bucket: bucketName,
+    WebsiteConfiguration: {
+      IndexDocument: {
+        Suffix: 'index.html'
+      }
+    }
+  }
 
   return new Promise((resolve, reject) => {
-    // Call putBucketWebsite
+    s3.putBucketWebsite(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
