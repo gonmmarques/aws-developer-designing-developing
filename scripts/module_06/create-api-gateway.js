b80c9974dca822a16c5d8b50767e0661c804a908
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-26 17:20:05 +0100

    Adding Creating an API Gateway

--- scripts/module_06/create-api-gateway.js
+++ scripts/module_06/create-api-gateway.js
@@ -1,10 +1,10 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
-// TODO: Create api gateway object
+const apiG = new AWS.APIGateway()
 const apiName = 'hamster-api'
 
 let apiData
@@ -23,41 +23,92 @@ createRestApi(apiName)
 .then(data => console.log(data))
 
 function createRestApi (apiName) {
-  // TODO: Create params const
+  const params = {
+    name: apiName
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Create a new rest API
+    apiG.createRestApi(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
 
 function getRootResource (api) {
-  // TODO: Create params const
+  const params = {
+    restApiId: api.id
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Get the resources and find the resource with path '/'
+    apiG.getResources(params, (err, data) => {
+      if (err) reject(err)
+      else {
+        const rootResource = data.items.find(r => r.path === '/')
+        resolve(rootResource.id)
+      }
+    })
   })
 }
 
 function createResource (parentResourceId, resourcePath, api) {
-  // TODO: Create params const
+  const params = {
+    parentId: parentResourceId,
+    pathPart: resourcePath,
+    restApiId: api.id
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Create the resource and return the resource id
+    apiG.createResource(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data.id)
+    })
   })
 }
 
 function createResourceMethod (resourceId, method, api, path) {
-  // TODO: Create params const
+  const params = {
+    authorizationType: 'NONE',
+    httpMethod: method,
+    resourceId: resourceId,
+    restApiId: api.id
+  }
+
+  if (path) {
+    params.requestParameters = {
+      [`method.request.path.${path}`]: true
+    }
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Put the method and return the resourceId argument
+    apiG.putMethod(params, (err) => {
+      if (err) reject(err)
+      else resolve(resourceId)
+    })
   })
 }
 
 function createMethodIntegration (resourceId, method, api, path) {
-  // TODO: Create params const
+  const params = {
+    httpMethod: method,
+    resourceId: resourceId,
+    restApiId: api.id,
+    integrationHttpMethod: method,
+    type: 'HTTP_PROXY',
+    uri: 'http://hamsterELB-1989668309.us-east-1.elb.amazonaws.com'
+  }
+
+  if (path) {
+    params.uri += `/{${path}}`
+    params.requestParameters = {
+      [`integration.request.path.${path}`]: `method.request.path.${path}`
+    }
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Put the integration and return the resourceId argument
+    apiG.putIntegration(params, (err) => {
+      if (err) reject(err)
+      else resolve(resourceId)
+    })
   })
 }
