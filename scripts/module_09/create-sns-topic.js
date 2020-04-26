6d1753dc6ba820d07fa5f255ba700461e041f4a7
Gonçalo Marques <goncalo.m.marques@tecnico.ulisboa.pt>
2020-04-26 19:32:56 +0100

    Adding Creating an SNS Topic

--- scripts/module_09/create-sns-topic.js
+++ scripts/module_09/create-sns-topic.js
@@ -1,19 +1,24 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
-// TODO: Create sns object
+const sns = new AWS.SNS()
 const topicName = 'hamster-topic'
 
 createTopic(topicName)
 .then(data => console.log(data))
 
 function createTopic (topicName) {
-  // TODO: Create params const
+  const params = {
+    Name: topicName
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Create topic
+    sns.createTopic(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
