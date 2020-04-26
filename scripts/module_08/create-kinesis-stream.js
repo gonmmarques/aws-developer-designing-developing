bf57c890a64201172412070e5690472a4c44ac82
Gonçalo Marques <goncalo.m.marques@tecnico.ulisboa.pt>
2020-04-26 19:14:34 +0100

    Adding Creating a Kinesis Stream

--- scripts/module_08/create-kinesis-stream.js
+++ scripts/module_08/create-kinesis-stream.js
@@ -1,19 +1,25 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
-// TODO: Create kinesis object
+const kinesis = new AWS.Kinesis()
 const streamName = 'hamster-race-results'
 
 createKinesisStream(streamName)
 .then(data => console.log(data))
 
 function createKinesisStream (streamName) {
-  // TODO: Create params const
+  const params = {
+    ShardCount: 1,
+    StreamName: streamName
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Create kinesis stream
+    kinesis.createStream(params, (err) => {
+      if (err) reject(err)
+      else resolve()
+    })
   })
 }
