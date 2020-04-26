4c218aeef8c0f507f54840c2fa823965cb8cbc75
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-26 19:35:42 +0100

    Adding Creating an SNS Subscription

--- scripts/module_09/create-sns-subscription.js
+++ scripts/module_09/create-sns-subscription.js
@@ -1,21 +1,28 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 const sns = new AWS.SNS()
 const type = 'sms'
-const endpoint = '/* TODO: Add your mobile number with country code */'
-const topicArn = '/* TODO: Add your sns topic arn */'
+const endpoint = '351'
+const topicArn = 'arn:aws:sns:us-east-1:773771193701:hamster-topic'
 
 createSubscription(type, topicArn, endpoint)
 .then(data => console.log(data))
 
 function createSubscription (type, topicArn, endpoint) {
-  // TODO: Create params const
+  const params = {
+    Protocol: type,
+    TopicArn: topicArn,
+    Endpoint: endpoint
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Subscribe
+    sns.subscribe(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
