75fb8c280c25d1a325da7b8a4f40d6bca9947947
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-26 17:54:44 +0100

    Update the S3 Bucket url to fix the API Gateway proxy redirect

--- util/assets.js
+++ util/assets.js
@@ -1,4 +1,4 @@
-const base = 'https://hamster-bucket-gm.s3.amazonaws.com'
+const base = 'https://s3.amazonaws.com/hamster-bucket-gm'
 module.exports = {
   hamster1: base + '/images/hamster-1-final.png',
   hamster2: base + '/images/hamster-2-final.png',
