390ccdf370c54697566d87ba284eeb651912a460
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-26 16:56:02 +0100

    Adding Creating Record Set in Route 53

--- scripts/module_06/create-route53-recordset.js
+++ scripts/module_06/create-route53-recordset.js
@@ -1,21 +1,42 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 const route53 = new AWS.Route53()
-const hzId = '/* TODO: Add your hostedzone id */'
+const hzId = '/hostedzone/Z01037791A2STBJW6E3WS'
 
 createRecordSet(hzId)
 .then(data => console.log(data))
 
 function createRecordSet (hzId) {
-  // TODO: Create params const
+  const params = {
+    HostedZoneId: hzId,
+    ChangeBatch: {
+      Changes: [
+        {
+          Action: 'CREATE',
+          ResourceRecordSet: {
+            Name: 'goncalommarques.codes',
+            Type: 'A',
+            AliasTarget: {
+              DNSName: 'hamsterELB-1989668309.us-east-1.elb.amazonaws.com',
+              EvaluateTargetHealth: false,
+              HostedZoneId: 'Z35SXDOTRQ7X7K'
+            }
+          }
+        }
+      ]
+    }
+  }
   // Link to ELB Regions:
   // https://docs.aws.amazon.com/general/latest/gr/elb.html
 
   return new Promise((resolve, reject) => {
-    // TODO: Create record set
+    route53.changeResourceRecordSets(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
