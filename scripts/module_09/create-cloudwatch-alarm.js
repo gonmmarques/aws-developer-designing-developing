4923f418dd107598c227f2abf2488a7b70b740b8
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-26 19:46:18 +0100

    Adding Creating a CloudWatch Alarm

--- scripts/module_09/create-cloudwatch-alarm.js
+++ scripts/module_09/create-cloudwatch-alarm.js
@@ -1,22 +1,47 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 const cw = new AWS.CloudWatch()
 const alarmName = 'hamster-elb-alarm'
-const topicArn = '/* TODO: Add your SNS topic ARN */'
-const tg = '/* TODO: Add last part of Target Group ARN */'
-const lb = '/* TODO: Add last part of Load Balancer ARN */'
+const topicArn = 'arn:aws:sns:us-east-1:773771193701:hamster-topic'
+const tg = 'targetgroup/hamsterTG/48aa0215a22f08d9'
+const lb = 'app/hamsterELB/07b8687bf3eb3ddc'
 
 createCloudWatchAlarm(alarmName, topicArn, tg, lb)
 .then(data => console.log(data))
 
 function createCloudWatchAlarm (alarmName, topicArn, tg, lb) {
-  // TODO: Create params const object
+  const params = {
+    AlarmName: alarmName,
+    ComparisonOperator: 'LessThanThreshold',
+    EvaluationPeriods: 1,
+    MetricName: 'HealthyHostCount',
+    Namespace: 'AWS/ApplicationELB',
+    Period: 60,
+    Threshold: 1,
+    AlarmActions: [
+      topicArn
+    ],
+    Dimensions: [
+      {
+        Name: 'TargetGroup',
+        Value: tg
+      }, {
+        Name: 'LoadBalancer',
+        Value: lb
+      }
+    ],
+    Statistic: 'Average',
+    TreatMissingData: 'breaching'
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Call putMetricAlarm
+    cw.putMetricAlarm(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
