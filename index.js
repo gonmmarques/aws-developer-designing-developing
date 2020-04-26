9745a13a8272d2571df5d875f8f6928dde177ccd
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-26 19:10:48 +0100

    Adding Sending and Polling messages in SQS

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const init = async () => {
   // initialize database and start server
   usersInit()
   // Commented out until SQS is configured
-  // .then(() => queueInit())
+ .then(() => queueInit())
   .then(async () => {
     try {
       await server.start()
