0000823f5cc8964bca64580022abe0996a5c80a0
Gonçalo Marques <goncalo.m.marques@tecnico.ulisboa.pt>
2020-04-26 18:15:51 +0100

    Adding Creating a CloudFront Distribution

--- scripts/module_07/cloudfront-parameters.js
+++ scripts/module_07/cloudfront-parameters.js
@@ -1,11 +1,46 @@
 function origins (bucketName) {
   return {
-    // TODO: Add properties for Origins
+    Quantity: 1,
+    Items: [
+      {
+        DomainName: `${bucketName}.s3.amazonaws.com`,
+        Id: `${bucketName}_origin`,
+        S3OriginConfig: {
+          OriginAccessIdentity: ''
+        }
+      }
+    ]
   }
 }
 function defaultCacheBehavior (bucketName) {
   return {
-    // TODO: Add properties for DefaultCacheBehavior
+    ForwardedValues: {
+      Cookies: {
+        Forward: 'none'
+      },
+      QueryString: false
+    },
+    MinTTL: 0,
+    TargetOriginId: `${bucketName}_origin`,
+    TrustedSigners: {
+      Quantity: 0,
+      Enabled: false
+    },
+    ViewerProtocolPolicy: 'redirect-to-https',
+    AllowedMethods: {
+      Quantity: 2,
+      Items: [
+          'GET',
+          'HEAD'
+      ],
+      CachedMethods: {
+        Quantity: 2,
+        Items: [
+          'GET',
+          'HEAD'
+        ]
+      }
+    }
   }
 }
 
--- scripts/module_07/create-cloudfront-distribution.js
+++ scripts/module_07/create-cloudfront-distribution.js
@@ -2,18 +2,32 @@
 const AWS = require('aws-sdk')
 const cfParams = require('./cloudfront-parameters')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
-// TODO: Create CloudFront SDK Object
+const cf = new AWS.CloudFront()
 
-createDistribution('/* TODO: Add your bucket name */')
+createDistribution('hamster-bucket-gm')
 .then(data => console.log(data))
 
 function createDistribution (bucketName) {
-  // TODO: Create params const object
+  const params = {
+    DistributionConfig: {
+      CallerReference: `${Date.now()}`,
+      Comment: 'HBFL Distribution',
+      DefaultCacheBehavior: cfParams.defaultCacheBehavior(bucketName),
+      Origins: cfParams.origins(bucketName),
+      HttpVersion: 'http2',
+      PriceClass: 'PriceClass_100',
+      IsIPV6Enabled: true,
+      Enabled: true,
+    }
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Call createDistribution
+    cf.createDistribution(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
