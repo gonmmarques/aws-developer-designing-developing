acf9c1c302deb281d1a14d1813ab7fcdcbfdb828
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-26 16:48:55 +0100

    Adding Creating Hosted Zone in Route 53

--- scripts/module_06/create-route53-hostedzone.js
+++ scripts/module_06/create-route53-hostedzone.js
@@ -1,19 +1,25 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
-// TODO: Create route53 object
-const hzName = 'hbfl.online'
+const route53 = new AWS.Route53()
+const hzName = 'goncalommarques.codes'
 
 createHostedZone(hzName)
 .then(data => console.log(data))
 
 function createHostedZone (hzName) {
-  // TODO: Create params const
+  const params = {
+    Name: hzName,
+    CallerReference: `${Date.now()}`
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Create hostedzone with route53
+    route53.createHostedZone(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
