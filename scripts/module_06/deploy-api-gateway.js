7d17da504e5f7e45d417d723d599f9061f2bb2bf
Gonçalo Marques <goncalo.marques@mercedes-benz.io>
2020-04-26 17:43:40 +0100

    Adding Deploying an API Gateway

--- scripts/module_06/deploy-api-gateway.js
+++ scripts/module_06/deploy-api-gateway.js
@@ -1,19 +1,25 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 const apiG = new AWS.APIGateway()
-const apiId = '/* TODO: Add api id */'
+const apiId = 'ufpa2osqn8'
 
 createDeployment(apiId, 'prod')
 .then(data => console.log(data))
 
 function createDeployment (apiId, stageName) {
-  // TODO: Create params const
+  const params = {
+    restApiId: apiId,
+    stageName: stageName
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Create deployment
+    apiG.createDeployment(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
